485e899e827acbe669274cb822f16b525541c9e6
ac <praveen.pavan0509@gmail.com>
2019-11-13 19:23:38 +0530

    my second commit by akhil

--- src/com/semantic/app/SampleProj.java
+++ src/com/semantic/app/SampleProj.java
@@ -10,6 +10,12 @@ public class SampleProj {
     }
     return rev;
   }
+
+  public static void add(){
+    int a=10 , b=20;
+    int c =a+b;
+    System.out.println("addition is ::"+c);
+  }
   public static void main(String[] args) {
     System.out.println("welcome to new got world");
 
@@ -18,5 +24,6 @@ public class SampleProj {
     String name = scn.nextLine();
     System.out.println("Given String is :: "+name);
 
+    SampleProj.add();
   }
 }
