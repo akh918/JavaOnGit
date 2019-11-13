323bdf67209ac05ee454044fab81aabb2759ebde
SandeepCherukuri <sandeep.cherukuri@india.semanticbits.com>
2019-11-13 19:30:37 +0530

    "subha_java_file_commit_04"

--- src/com/semantic/app/SampleProj.java
+++ src/com/semantic/app/SampleProj.java
@@ -3,5 +3,7 @@ package com.semantic.app;
 public class SampleProj {
     public static void main(String[] args) {
         System.out.println("welcome to new got world");
+        int a=10;
+        System.out.println(++a);
     }
 }
