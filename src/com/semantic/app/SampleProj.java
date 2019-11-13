976541bfc253d60c892479002a7e9488987b8f9b
saikumarpuli <saikumarpuli9@gmail.com>
2019-11-13 19:29:51 +0530

    Latha commit

--- src/com/semantic/app/SampleProj.java
+++ src/com/semantic/app/SampleProj.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 package com.semantic.app;
 
 public class SampleProj {
-    public static void main(String[] args) {
-        System.out.println("welcome to new got world");
+
+  public static String reverseString(String str){
+    String rev = "";
+    for(int i=str.length()-1; i>=0; i--){
+      rev = rev + str.charAt(i);
     }
+    return rev;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("welcome to new got world");
+
+    Scanner scn = new Scanner(System.in);
+    System.out.println("Enter any string");
+    String name = scn.nextLine();
+    System.out.println("Given String is :: "+name);
+    String reverse = SampleProgram01.reverseString(name);
+    System.out.println("Reverse String is :: "+reverse);
+  }
 }
