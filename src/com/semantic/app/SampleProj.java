499d76848ac842eff08dfeed670613ea427b8e81
ac <praveen.pavan0509@gmail.com>
2019-11-13 18:57:00 +0530

    my first commit by akhildevelopwe

--- src/com/semantic/app/SampleProj.java
+++ src/com/semantic/app/SampleProj.java
@@ -3,5 +3,6 @@ package com.semantic.app;
 public class SampleProj {
     public static void main(String[] args) {
         System.out.println("welcome to new got world");
+        System.out.println("develope branch");
     }
 }
